39ba4c6b11a1373b598d2f5a29b5817bd079aef9
marvin-delansorne <marvin.delansorne@laplateforme.io>
2025-02-04 11:12:53 +0100

    Add files via upload

---
+++ Les_classes/job01/index.php
@@ -0,0 +1,23 @@
+
+
+<?php
+
+class Operation {
+   
+    public $nombre1;
+    public $nombre2;
+
+    
+    public function __construct($nombre1 = 0, $nombre2 = 0) {
+        
+        $this->nombre1 = $nombre1;
+        $this->nombre2 = $nombre2;
+    }
+}
+
+$operation = new Operation();
+
+
+echo "Nombre 1: " . $operation->nombre1 . "<br>";
+echo "Nombre 2: " . $operation->nombre2 . "<br>";
+?>
