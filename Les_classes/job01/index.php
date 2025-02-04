11e29534559fe2fbbad7bb668d839409752b82dd
Your Name <marvin.delansorne@laplateforme.io>
2025-02-04 14:31:53 +0100

    Job 01 jour01 Structure de classe

--- Les_classes/job01/index.php
+++ Les_classes/job01/index.php
@@ -1,21 +1,15 @@
-<?php
-
-class Operation {
-   
+<?php 
+ class Operation{
     public $nombre1;
     public $nombre2;
 
-    
-    public function __construct($nombre1 = 0, $nombre2 = 0) {
-        
+    public function __construct($nombre1, $nombre2) {
         $this->nombre1 = $nombre1;
-        $this->nombre2 = $nombre2;
+       $this->nombre2 = $nombre2;
     }
-}
+ }
 
-$operation = new Operation();
+ $monOperation = new Operation(0, 0);
 
-
-echo "Nombre 1: " . $operation->nombre1 . "<br>";
-echo "Nombre 2: " . $operation->nombre2 . "<br>";
-?>
+ echo $monOperation->nombre1;
+ echo $monOperation->nombre2;
